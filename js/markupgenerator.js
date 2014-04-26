6371f85d45f7fbe3bda184b6e73488dac4f12cac
Putzmitt3l <dkralev90@gmail.com>
2014-04-26 22:40:53 +0300

    Adding onCheckboxCheck/Uncheck selctedExcerciseTags to appear/dissappear in the "To Export" tag-container

---
+++ js/markupgenerator.js
@@ -0,0 +1,3 @@
+function generateSelectedExcerciseTagMarkup(id, name) {
+	return '<li id="' + id + '" class="exported-item border-round3"><p class="tag"><span class="item-name">' + name + '</span><button class="remove-exported-item-button">X</button></p></li>';
+}
--- js/ui.js
+++ js/ui.js
@@ -1,15 +1,35 @@
 (function ($) {
-	$exportedItemsList = $('.exported-items'),
-	$overlay = $('.overlay');
+	var $overlay = $('.overlay');
 
 	$('.add-checkbox').on('click',function(e) {
 		var $checktedCheckBox = $(this),
-			$selectedMatchedItem = $checktedCheckBox.parent();
+			$selectedMatchedItem = $checktedCheckBox.parent(),
+			matchedItemId = $selectedMatchedItem.attr('id')
+			matchedItemName = $selectedMatchedItem.children('.match-name').text();
 		$checktedCheckBox.toggleClass('checkbox-checked');
 
-		//find in exportedItemsList child with Id === $selectedMatchedItem.getId()
-		//if found remove it
-		//if not-found add it
+		var $existinsMatchedItemExercseTag = $('.exported-items').children('#'+ matchedItemId),
+			selectedExcerciseTag = generateSelectedExcerciseTagMarkup(matchedItemId, matchedItemName),		
+			$container = $('.exported-items');
+
+		if($existinsMatchedItemExercseTag.length > 0) { //tag already exists
+			var contents = $('.exported-items').html(),
+				newStringContents = contents.replace( selectedExcerciseTag + ',', '')
+											.replace(','+ selectedExcerciseTag + ',' , '')
+											.replace(','+ selectedExcerciseTag, '')
+											.replace(selectedExcerciseTag, '');
+			$container.empty();
+			$container.append(newStringContents);
+		}
+		else {
+			
+			if($container.children().length > 0) {
+				$container.append(',' + selectedExcerciseTag);
+			}
+			else {
+				$container.append(selectedExcerciseTag);
+			}
+		}
 	});
 
 	$('.close-match-desc').on('click', function(e){
