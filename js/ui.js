e362590a26243683df10acaebaa82e2f651d5c37
Putzmitt3l <dkralev90@gmail.com>
2014-04-26 21:54:15 +0300

    Adding excersize-description overlay functionality

--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,30 @@
 (function ($) {
+	$exportedItemsList = $('.exported-items'),
+	$overlay = $('.overlay');
+
 	$('.add-checkbox').on('click',function(e) {
-		$(this).toggleClass('checkbox-checked');
+		var $checktedCheckBox = $(this),
+			$selectedMatchedItem = $checktedCheckBox.parent();
+		$checktedCheckBox.toggleClass('checkbox-checked');
+
+		//find in exportedItemsList child with Id === $selectedMatchedItem.getId()
+		//if found remove it
+		//if not-found add it
+	});
+
+	$('.close-match-desc').on('click', function(e){
+		$overlay.addClass('hidden');
+	});
+
+	$('.overlay-background').on('click', function(e){
+		$overlay.addClass('hidden');
+	});	
+
+	$('.open-decription').on('click', function (e){
+		$overlay.removeClass('hidden');
+	});
+
+	$('.export-to-gitbook-button').on('click', function(e){
+		//do exporting shit here
 	});
 })(jQuery);
